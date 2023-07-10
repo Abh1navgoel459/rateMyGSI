a548568a12ad6435e998c6bb9340ae1659981055
Abh1navgoel459 <123058674+Abh1navgoel459@users.noreply.github.com>
2023-07-10 14:39:52 -0400

    Update submit_rating.php

--- submit_rating.php
+++ submit_rating.php
@@ -9,10 +9,16 @@ $username = "student";
 $password = "student123456789";
 $dbname = "teacher_rating_system";
 
-session_start(); 
+session_start();
 $student_id = $_SESSION['student_id'];
-$teacher_id = $_GET["teacher_id"];
-$stars = $_GET["stars"];
+$teacher_id = $_POST["teacher_id"];
+$overallRating = $_POST["overallRating"];
+$difficultyRating = $_POST["difficultyRating"];
+$takeAgain = $_POST["takeAgain"];
+$credit = $_POST["credit"];
+$attendance = $_POST["attendance"];
+$textbooks = $_POST["textbooks"];
+$grade = $_POST["grade"];
 
 $conn = mysqli_connect('127.0.0.1', 'student', 'student123456789', 'teacher_rating_system');
 
@@ -21,7 +27,7 @@ $sql = "SELECT * FROM student_teacher WHERE student_id =$student_id AND teacher_
 $result = mysqli_query($conn, $sql);
 if (mysqli_num_rows($result) == 0) {
     // Insert rating data
-    $sql = "INSERT INTO student_teacher(teacher_id, avg_rating, student_id, update_dt) VALUES ($teacher_id, '$stars', $student_id, curdate())";
+    $sql = "INSERT INTO student_teacher(teacher_id, avg_rating, student_id, update_dt, difficulty_rating, take_again, credit, attendance, textbooks, grade) VALUES ($teacher_id, '$overallRating', $student_id, curdate(), '$difficultyRating', '$takeAgain', '$credit', '$attendance', '$textbooks', '$grade')";
     $rs = mysqli_query($conn, $sql);
     if ($rs) {
     echo "Rating submitted successfully";
@@ -37,15 +43,25 @@ if (mysqli_num_rows($result) == 0) {
         echo "Error: " . $sql . "<br>" . $conn->error;
     }
 } else {
-    $sql = "UPDATE student_teacher set avg_rating = '$stars' where student_id = $student_id and teacher_id = $teacher_id";
+    $sql = "UPDATE student_teacher SET avg_rating = '$overallRating', difficulty_rating = '$difficultyRating', take_again = '$takeAgain', credit = '$credit', attendance = '$attendance', textbooks = '$textbooks', grade = '$grade' WHERE student_id = $student_id AND teacher_id = $teacher_id";
     $rs = mysqli_query($conn, $sql);
     if ($rs) {
         echo "Rating updated successfully!";
-        $sql = " UPDATE teacher_ratings
+        $sql = "UPDATE teacher_ratings
         SET avg_rating = (
             SELECT AVG(avg_rating)
             FROM student_teacher
             WHERE teacher_id = '$teacher_id'
+        ),
+        diff_rating = (
+            SELECT AVG(difficulty_rating)
+            FROM student_teacher
+            WHERE teacher_id = '$teacher_id'
+        ),
+        repeat_gsi = (
+            SELECT (COUNT(*) / (SELECT COUNT(*) FROM student_teacher WHERE teacher_id = '$teacher_id')) * 100
+            FROM student_teacher
+            WHERE teacher_id = '$teacher_id' AND take_again = 'yes'
         )
         WHERE teacher_id = '$teacher_id';";
         $rs = mysqli_query($conn, $sql);
